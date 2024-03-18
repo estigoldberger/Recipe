d8aece9a2ccf94af642ceab48f778fbc0f6e446c
estigoldberger <estigabb@gmail.com>
2024-03-18 16:04:34 -0400

    #48

---
+++ functions/MealCalorieCount.sql
@@ -0,0 +1,19 @@
+create or alter function dbo.MealCalorieCount(@MealId int)
+returns int
+as 
+begin 
+	declare @value int=0
+	select @value= SUM(r.Calorie)
+    from meal m 
+    join MealCourse mc
+    on m.MealID= mc.mealID
+    join RecipeMealCourse rmc 
+    on mc.MealCourseID=rmc.MealCourseId
+    join recipe r 
+    on r.RecipeId=rmc.RecipeID
+    where m.MealID= @MealId
+    group by m.MealName
+    order by m.MealName
+    return @value
+end
+go 
---
+++ functions/RecipeInfo.sql
@@ -0,0 +1,25 @@
+create or alter function dbo.RecipeInfo(@RecipeId int)
+returns varchar(85)
+as 
+begin 
+	declare @value varchar(85)=''
+
+	select @value=concat(r.RecipeName, ' (', c.CuisineType, ') ', 'has ', count(distinct ri.RecipeSequence), ' ingredients and ', count(distinct rd.DirectionSequence), ' steps') 
+    from cuisine c 
+    left join recipe r 
+    on c.CuisineID= r.CuisineID
+    join RecipeIngredient ri
+    on r.RecipeId= ri.RecipeID
+    join RecipeDirection rd 
+    on r.RecipeId= rd.RecipeID
+    where r.RecipeId= @RecipeId
+    group by r.RecipeName, c.CuisineType
+    
+    
+    return @value
+end
+go 
+
+/*select dbo.RecipeInfo(r.RecipeId)
+from recipe r*/
+
