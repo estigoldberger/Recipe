4623f5d3e8761fa16e4adcc42497b177549e505b
estigoldberger <estigabb@gmail.com>
2024-03-26 10:50:14 -0400

    #48

--- functions/MealCalorieCount.sql
+++ functions/MealCalorieCount.sql
@@ -4,19 +4,17 @@ as
 begin 
 	declare @value int=0
 	select @value= SUM(r.Calorie)
-    from meal m 
-    join MealCourse mc
-    on m.MealID= mc.mealID
+    from  MealCourse mc
     join RecipeMealCourse rmc 
     on mc.MealCourseID=rmc.MealCourseId
     join recipe r 
     on r.RecipeId=rmc.RecipeID
-    where m.MealID= @MealId
-    group by m.MealName
-    order by m.MealName
+    where mc.MealID= @MealId
+
     return @value
 end
 go 
 /*select dbo.MealCalorieCount(m.mealid)
 from meal m 
-where m.mealname= 'breakfast bash'*/
+where m.mealname= 'supper sensation'
+*/
